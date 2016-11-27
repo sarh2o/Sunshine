3a6fb0853a30b716ddb273514a824b8bf37e6c04
Alex Liu <lanshaibaobei0820@gmail.com>
2016-11-27 22:27:58 +0800

    dd listview

--- app/src/main/java/com/example/sarh2o/sunshine/PlaceholderFragment.java
+++ app/src/main/java/com/example/sarh2o/sunshine/PlaceholderFragment.java
@@ -7,6 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -62,6 +68,25 @@ public class PlaceholderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+        List<String> forecastItems = new ArrayList<String>(Arrays.<String>asList(data));
+        ArrayAdapter<String> forecastViewAdapter =
+                new ArrayAdapter<>(getActivity(),
+                        R.layout.list_item_forecast,
+                        R.id.list_item_forecast_textView,
+                        forecastItems);
+        ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecastViewAdapter);
+
+        return rootView;
     }
 }
